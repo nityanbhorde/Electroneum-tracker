fda03ae6904b94f7df9ecf10845149fe3cbce865
Nityan <bhordenityan@hotmail.com>
2018-01-20 02:41:14 -0800

    Requesting multiple times to api

--- www/mining.js
+++ www/mining.js
@@ -3,50 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//globals
 var storage;
-var base_api = "https://api.etn.spacepools.org/v1/stats/address/";
+var average =0;
+var data;
 
 
 function init() {
-    document.addEventListener("deviceready",onDeviceReady,false);
     storage = window.localStorage;
-}
-
-function onDeviceReady(){
-    var node = document.createElement('link');
-    
-    node.setAttribute('rel','stylesheet');
-    node.setAttribute('type','text/css');
-    node.setAttribute('href','style.css');
-    
-    document.getElementsByTagName('head')[0].appendChild(node);
 }
 // on etn submit handler
 function processForm(){
+    var base_api = "https://api.etn.spacepools.org/v1/stats/address/";
     var info = document.getElementById("info");
-    var url = base_api.concat(info.value);
+    url = base_api.concat(info.value);
+    requestData();
+    setTimeout(printAverage,2000);
+}
+// calculate average hash rate from past 10 instances
+function calc_avg_hash(){
+    var array_length = data.charts.hashrate.length;
+    var i = array_length -1;
+    while(i > array_length -11){
+        average += data.charts.hashrate[i]["1"];
+        i--;
+    }
+    average /= 10;
+}
+function printAverage(){
+    var space = document.getElementById("inserting");
+    var htmlString = "";
+    htmlString = "<p> Your average hash rate is " + average + "! </p>";
+    space.insertAdjacentHTML('beforeend',htmlString);
+}
+//requests data from api every ten minutes, makes sure rate has not dropped
+function requestData(){
     var request = new XMLHttpRequest();
     request.open('GET',url);
     request.onload = function(){
-        var data = JSON.parse(request.responseText);
-        console.log(data);
-        //console.log(data.charts.hashrate["0"]);
-        calc_avg_hash(data);
+        data = JSON.parse(request.responseText);
+        var curr = data.charts.hashrate.length -1;
+        console.log(data.charts.hashrate[curr]["1"]);
+        console.log(average);
+        if(data.charts.hashrate[curr]["1"] < average){
+            console.log("hello");
+        }
+       calc_avg_hash();
     };
     request.send();
-}
-// calculate average hash rate from past 10 instances
-function calc_avg_hash(data){
-    var array_length = data.charts.hashrate.length;
-    var i = array_length -1;
-    var accum = 0;
-    while(i > array_length -11){
-        accum += data.charts.hashrate[i]["1"];
-        i--;
-    }
-    accum /= 10;
-    var space = document.getElementById("inserting");
-    var htmlString = "";
-    htmlString = "<p> Your average hash rate is " + accum + "! </p>";
-    space.insertAdjacentHTML('beforeend',htmlString);
+        
+    setTimeout(requestData,10000); //600000
 }
